7a9ba4c833e4ce199f0e697b76810830deaa9179
Simon Taylor <simontaylor81@hotmail.com>
2014-01-07 20:19:43 +0000

    Initial commit, with very basic outline of site.

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+﻿// Hash Password functionality.
+
+$(function () {
+    // Hook up submit event.
+    $('#main-form').submit(function (event) {
+        // Don't do the regular submit, as it refreshes the page.
+        event.preventDefault();
+
+        var site = $('#site-id').val();
+        var masterPwd = $('#master-pwd').val();
+        var sitePwd = generatePassword(site, masterPwd);
+        $('#site-pwd').text(sitePwd);
+    });
+});
+
+function generatePassword(siteId, masterPwd) {
+    return siteId + masterPwd;
+}
